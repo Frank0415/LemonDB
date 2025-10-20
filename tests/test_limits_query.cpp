5a4b401924de52de21808d883eed61e1dac3cb6c
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-21 03:29:51 +0800

    test(p2m1): extended test LIMITS

--- tests/test_limits_query.cpp
+++ tests/test_limits_query.cpp
@@ -20,6 +20,14 @@ TEST(LimitsQueryTest, MaxAndMinSameTableNoCondition)
     table.insertByIndex("s1", std::vector<TableV>{112, 2014});
     table.insertByIndex("s2", std::vector<TableV>{150, 2016});
     table.insertByIndex("s3", std::vector<TableV>{100, 2013});
+    // add many more rows (s4..s53) that do not change min/max
+    for (int i = 4; i <= 53; ++i)
+    {
+        std::string key = "s" + std::to_string(i);
+        TableV credit = 120 + (i % 20); // between 120..139
+        TableV cls = 2000 + (i % 20);   // between 2000..2019
+        table.insertByIndex(key, std::vector<TableV>{credit, cls});
+    }
 
     // prepare operands and empty condition
     std::vector<std::string> operands = {"totalCredit", "class"};
@@ -31,7 +39,7 @@ TEST(LimitsQueryTest, MaxAndMinSameTableNoCondition)
     EXPECT_TRUE(rmax->success());
     std::ostringstream oss;
     oss << *rmax;
-    EXPECT_EQ(oss.str(), "ANSWER = ( 150 2016 )\n");
+    EXPECT_EQ(oss.str(), "ANSWER = ( 150 2019 )\n");
 
     // MIN
     auto qmin = std::make_unique<MinQuery>("limits_test", operands, conds);
@@ -39,7 +47,7 @@ TEST(LimitsQueryTest, MaxAndMinSameTableNoCondition)
     EXPECT_TRUE(rmin->success());
     std::ostringstream oss2;
     oss2 << *rmin;
-    EXPECT_EQ(oss2.str(), "ANSWER = ( 100 2013 )\n");
+    EXPECT_EQ(oss2.str(), "ANSWER = ( 100 2000 )\n");
 
     // clean up
     Database::getInstance().dropTable("limits_test");
