df1e7784f44d909e7ff10f9c2cbb7843b61f4283
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-21 02:58:50 +0800

    fix(p2m1): fixed misc issues in MAX implementation

--- src/query/data/MaxQuery.cpp
+++ src/query/data/MaxQuery.cpp
@@ -15,72 +15,91 @@ QueryResult::Ptr MaxQuery::execute()
             "No operand (? operands)."_f % operands.size());
     }
 
-    Database &db = Database::getInstance();
     try
     {
+        Database &db = Database::getInstance();
         auto &table = db[this->targetTable];
-        auto result = initCondition(table);
-        if (result.second)
-        {
-            bool found = false;
-            vector<Table::ValueType> maxValue; // each has its own max value
-            for (size_t i = 0; i < this->operands.size(); i++)
-            {
-                maxValue.push_back(Table::ValueTypeMin);
-            }
-            string resultStr = "MAX RESULT : ( ";
 
-            for (auto it = table.begin(); it != table.end(); ++it)
+        vector<Table::FieldIndex> fieldId; // transform into its own Id, avoid lookups in map everytime
+
+        try
+        {
+            for (const auto &operand : this->operands)
             {
-                if (this->evalCondition(*it))
+                if (operand == "KEY")
                 {
-                    for (size_t i = 0; i < maxValue.size(); ++i)
+                    throw IllFormedQueryCondition("MAX operation not supported on KEY field.");
+                }
+                fieldId.push_back(table.getFieldIndex(operand));
+            }
+        }
+        catch (const TableFieldNotFound &e)
+        {
+            return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
+        }
+        catch (const exception &e)
+        {
+            return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                               "Unkonwn error '?'."_f % e.what());
+        }
+
+        try
+        {
+            auto result = initCondition(table);
+            if (result.second)
+            {
+                bool found = false;
+                vector<Table::ValueType> maxValue(
+                    fieldId.size(),
+                    Table::ValueTypeMin); // each has its own max value
+
+                for (auto it = table.begin(); it != table.end(); ++it)
+                {
+                    if (this->evalCondition(*it))
                     {
-                        if ((*it)[table.getFieldIndex(this->operands[i])] >
-                            maxValue[i])
+                        found = true;
+
+                        for (size_t i = 0; i < fieldId.size(); ++i)
                         {
-                            found = true;
-                            maxValue[i] = (*it)[table.getFieldIndex(this->operands[i])];
+                            if ((*it)[fieldId[i]] > maxValue[i])
+                            {
+                                maxValue[i] = (*it)[fieldId[i]];
+                            }
                         }
                     }
                 }
-            }
 
-            for (size_t i = 0; i < maxValue.size(); i++)
-            {
-                resultStr += to_string(maxValue[i]) + " ";
+                if (found == false)
+                {
+                    return make_unique<NullQueryResult>();
+                }
+                return make_unique<SuccessMsgResult>(maxValue);
             }
-
-            resultStr += ")";
-            if (found == false)
+            else
             {
+                return make_unique<NullQueryResult>();
             }
-            return make_unique<SuccessMsgResult>(qname, resultStr);
         }
-        else
+        catch (const IllFormedQueryCondition &e)
         {
-            return make_unique<NullQueryResult>();
+            return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
+        }
+        catch (const invalid_argument &e)
+        {
+            // Cannot convert operand to string
+            return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                               "Unknown error '?'"_f % e.what());
+        }
+        catch (const exception &e)
+        {
+            return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                               "Unkonwn error '?'."_f % e.what());
         }
     }
     catch (const TableNameNotFound &e)
     {
         return make_unique<ErrorMsgResult>(qname, this->targetTable,
                                            "No such table."s);
-    }
-    catch (const IllFormedQueryCondition &e)
-    {
-        return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
-    }
-    catch (const invalid_argument &e)
-    {
-        // Cannot convert operand to string
-        return make_unique<ErrorMsgResult>(qname, this->targetTable,
-                                           "Unknown error '?'"_f % e.what());
-    }
-    catch (const exception &e)
-    {
-        return make_unique<ErrorMsgResult>(qname, this->targetTable,
-                                           "Unkonwn error '?'."_f % e.what());
     }
 }
 
