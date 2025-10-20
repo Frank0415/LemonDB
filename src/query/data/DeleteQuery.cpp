0ac58ebd5eec3076bbcead569451c69a0fab2e0d
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-20 23:19:49 +0800

    fix(p2m1): fixed DELETE method's conditional

--- src/query/data/DeleteQuery.cpp
+++ src/query/data/DeleteQuery.cpp
@@ -8,19 +8,32 @@ constexpr const char *DeleteQuery::qname;
 QueryResult::Ptr DeleteQuery::execute()
 {
     using namespace std;
-    if (this->operands.empty())
-    {
-        return make_unique<ErrorMsgResult>(qname, this->targetTable.c_str(),
-                                           "No operand (? operands)."_f %
-                                               operands.size());
-    }
+    // if (this->operands.empty())
+    // {
+    //     return make_unique<ErrorMsgResult>(qname, this->targetTable.c_str(),
+    //                                        "No operand (? operands)."_f %
+    //                                            operands.size());
+    // }
     Database &db = Database::getInstance();
+    Table::SizeType counter = 0;
     try
     {
         auto &table = db[this->targetTable];
-        auto &key = this->operands.front();
-        table.deleteByIndex(key);
-        return std::make_unique<SuccessMsgResult>(qname, targetTable);
+        auto result = initCondition(table);
+        if (result.second)
+        {
+            for (auto it = table.begin(); it != table.end(); it++)
+            {
+                if (this->evalCondition(*it))
+                {
+                    auto toDeleteKey = it->key();
+                    ++it;
+                    table.deleteByIndex(toDeleteKey);
+                    ++counter;
+                }
+            }
+        }
+        return std::make_unique<SuccessMsgResult>(counter);
     }
     catch (const NotFoundKey &e)
     {
