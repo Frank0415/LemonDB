56227c575ba69861a37eb1d8716ae3bd718eda6e
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-22 19:28:50 +0800

    feat(p2m1-1): AddQuery

--- src/query/data/AddQuery.cpp
+++ src/query/data/AddQuery.cpp
@@ -17,6 +17,31 @@ QueryResult::Ptr AddQuery::execute() {
     auto &db = Database::getInstance();
     auto &table = db[this->targetTable];
 
+    auto result = initCondition(table);
+    if (!result.second) {
+      throw IllFormedQueryCondition("Error conditions in WHERE clause.");
+    }
+    // this operands stores a list of ADD ( fields ... destField ) FROM table
+    // WHERE ( cond ) ...;
+
+    // Stategy: iterate through all rows that satisfy the condition and sum all
+    // @ once
+    int count = 0;
+
+    for (auto it = table.begin(); it != table.end(); ++it) {
+      if (!this->evalCondition(*it)) {
+        continue;
+      }
+      // perform ADD operation
+      int sum = 0;
+      for (size_t i = 0; i < this->operands.size() - 1; ++i) {
+        auto fieldIndex = table.getFieldIndex(this->operands[i]);
+        sum += (*it)[fieldIndex];
+      }
+      (*it)[table.getFieldIndex(this->operands.back())] = sum;
+      count++;
+    }
+    return make_unique<RecordCountResult>(count);
   } catch (const NotFoundKey &e) {
     return make_unique<ErrorMsgResult>(qname, this->targetTable,
                                        "Key not found."s);
