271099206e0d3677f97ac6b466a1bc04ae7dca35
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-22 19:00:57 +0800

    feat(p2m1-1): SubQuery

--- src/query/data/SubQuery.cpp
+++ src/query/data/SubQuery.cpp
@@ -6,7 +6,34 @@
 
 constexpr const char *SubQuery::qname;
 
-QueryResult::Ptr SubQuery::execute() {}
+QueryResult::Ptr SubQuery::execute() {
+  using namespace std;
+  try {
+    if (this->operands.size() < 3) {
+      return make_unique<ErrorMsgResult>(
+          qname, this->targetTable,
+          "Invalid number of operands (? operands)."_f % operands.size());
+    }
+    auto &db = Database::getInstance();
+    auto &table = db[this->targetTable];
+
+  } catch (const NotFoundKey &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "Key not found."s);
+  } catch (const TableNameNotFound &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "No such table."s);
+  } catch (const IllFormedQueryCondition &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
+  } catch (const invalid_argument &e) {
+    // Cannot convert operand to string
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "Unknown error '?'"_f % e.what());
+  } catch (const exception &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "Unkonwn error '?'."_f % e.what());
+  }
+}
 
 std::string SubQuery::toString() {
   return "QUERY = Duplicate TABLE \"" + this->targetTable + "\"";
