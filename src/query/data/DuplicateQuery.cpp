e47cfc2a1a8d0471fdf43bb2a152c455ff785117
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-22 13:48:19 +0800

    feat(p2m1-1): Duplicate Query error handling

--- src/query/data/DuplicateQuery.cpp
+++ src/query/data/DuplicateQuery.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include <algorithm>
+
 #include "DuplicateQuery.h"
 #include "../../db/Database.h"
 #include "../QueryResult.h"
@@ -40,6 +41,31 @@ QueryResult::Ptr DuplicateQuery::execute()
                 }
             }
         }
+    }
+    catch (const NotFoundKey &e)
+    {
+        return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                           "Key not found."s);
+    }
+    catch (const TableNameNotFound &e)
+    {
+        return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                           "No such table."s);
+    }
+    catch (const IllFormedQueryCondition &e)
+    {
+        return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
+    }
+    catch (const invalid_argument &e)
+    {
+        // Cannot convert operand to string
+        return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                           "Unknown error '?'"_f % e.what());
+    }
+    catch (const exception &e)
+    {
+        return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                           "Unkonwn error '?'."_f % e.what());
     }
 }
 
