b67bece92185d0c8cda8483e211272f493e47c69
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-27 19:32:12 +0800

    build(p2m1-1): basic test sh script [test]

--- test/run.sh
+++ test/run.sh
@@ -2,13 +2,14 @@
 
 pwd
 echo "1"
-cat CPPLINT.cfg
+cmake -S . -B build -DCMAKE_CXX_COMPILER=clang++
+cmake --build build -j8
 echo "2"
 ls -la
 echo "3"
 echo "void"
 echo "4"
-ls /test/data/queries
+ls test/data/queries
 echo "5"
 
 ls test/data
@@ -17,3 +18,11 @@ echo "6"
 mkdir test/data/tmp
 ls test/data/tmp
 echo "7"
+cd ..
+ls -la
+pwd
+echo "8"
+cd /
+ls -la
+pwd
+echo "9"
