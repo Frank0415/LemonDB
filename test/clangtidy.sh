ddbc17199d15ccf4105b722a7cdcc6cfdd42678f
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-10-28 23:21:48 +0800

    fix(p2m1-1):  fix quality [test]

--- test/clangtidy.sh
+++ test/clangtidy.sh
@@ -7,7 +7,7 @@ compile_commands_path="../build/compile_commands.json"
 
 # Run clang-tidy on each file
 for file in $files; do
-  clang-tidy "$file" -p="$compile_commands_path" -checks='-*,clang-analyzer-*,-clang-analyzer-security*,-clang-analyzer-alpha*,bugprone-suspicious-string-compare,google-global-names-in-headers,misc-include-cleaner,misc-const-correctness,performance-*' \
+  clang-tidy-18 "$file" -p="$compile_commands_path" -checks='-*,clang-analyzer-*,-clang-analyzer-security*,-clang-analyzer-alpha*,bugprone-suspicious-string-compare,google-global-names-in-headers,misc-include-cleaner,misc-const-correctness,performance-*' \
   -warnings-as-errors='codequality-no-header-guard,cppcoreguidelines-init-variables,readability-redundant-*,performance' \
   --extra-arg=-D__clang_analyzer__ \
   --extra-arg-before=-std=c++20 \
