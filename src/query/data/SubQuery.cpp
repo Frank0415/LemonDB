817cb4fe850438d94bbbc19ec3ba5d44186547ce
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-03 14:14:59 +0800

    feat(p2m2-1): sub query async

--- src/query/data/SubQuery.cpp
+++ src/query/data/SubQuery.cpp
@@ -78,3 +78,98 @@ std::string SubQuery::toString()
 {
   return "QUERY = SUB TABLE \"" + this->targetTable + "\"";
 }
+
+[[nodiscard]] QueryResult::Ptr SubQuery::validateOperands() const
+{
+  if (this->operands.size() < 2)
+  {
+    return std::make_unique<ErrorMsgResult>(
+        qname, this->targetTable, "Invalid number of operands (? operands)."_f % operands.size());
+  }
+  return nullptr;
+}
+
+[[nodiscard]] std::vector<Table::FieldIndex> SubQuery::getFieldIndices(const Table& table) const
+{
+  std::vector<Table::FieldIndex> indices;
+  indices.reserve(this->operands.size());
+  for (const auto& operand : this->operands)
+  {
+    indices.push_back(table.getFieldIndex(operand));
+  }
+  return indices;
+}
+
+[[nodiscard]] QueryResult::Ptr
+SubQuery::executeSingleThreaded(Table& table, const std::vector<Table::FieldIndex>& fids)
+{
+  int count = 0;
+
+  for (auto it = table.begin(); it != table.end(); ++it)
+  {
+    if (!this->evalCondition(*it))
+    {
+      continue;
+    }
+    // perform SUB operation
+    int diff = (*it)[fids[0]];
+    for (size_t i = 1; i < this->operands.size() - 1; ++i)
+    {
+      diff -= (*it)[fids[i]];
+    }
+    (*it)[fids.back()] = diff;
+    count++;
+  }
+  return std::make_unique<RecordCountResult>(count);
+}
+
+[[nodiscard]] QueryResult::Ptr
+SubQuery::executeMultiThreaded(Table& table, const std::vector<Table::FieldIndex>& fids)
+{
+  constexpr size_t CHUNK_SIZE = Table::splitsize();
+  ThreadPool& pool = ThreadPool::getInstance();
+  std::vector<std::future<int>> futures;
+  futures.reserve((table.size() + CHUNK_SIZE - 1) / CHUNK_SIZE);
+
+  auto iterator = table.begin();
+  while (iterator != table.end())
+  {
+    auto chunk_start = iterator;
+    size_t count = 0;
+    while (iterator != table.end() && count < CHUNK_SIZE)
+    {
+      ++iterator;
+      ++count;
+    }
+    auto chunk_end = iterator;
+    futures.push_back(pool.submit(
+        [this, chunk_start, chunk_end, fids]()
+        {
+          int local_count = 0;
+          for (auto it = chunk_start; it != chunk_end; ++it)
+          {
+            if (!this->evalCondition(*it))
+            {
+              continue;
+            }
+            // perform SUB operation
+            int diff = (*it)[fids[0]];
+            for (size_t i = 1; i < this->operands.size() - 1; ++i)
+            {
+              diff -= (*it)[fids[i]];
+            }
+            (*it)[fids.back()] = diff;
+            local_count++;
+          }
+          return local_count;
+        }));
+  }
+
+  // Wait for all tasks to complete and aggregate results
+  int total_count = 0;
+  for (auto& future : futures)
+  {
+    total_count += future.get();
+  }
+  return std::make_unique<RecordCountResult>(total_count);
+}
