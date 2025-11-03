5d6c1e46a5be03890a90d747986230e1f34fc4c7
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-03 14:14:27 +0800

    feat(p2m2-1): sub query async

--- src/query/data/SubQuery.h
+++ src/query/data/SubQuery.h
@@ -10,12 +10,28 @@ class SubQuery : public ComplexQuery
 {
   static constexpr const char* qname = "SUB";
 
+private:
+  [[nodiscard]] QueryResult::Ptr validateOperands() const;
+
+  [[nodiscard]] std::vector<Table::FieldIndex> getFieldIndices(const Table& table) const;
+
+  [[nodiscard]] QueryResult::Ptr executeSingleThreaded(Table& table,
+                                                       const std::vector<Table::FieldIndex>& fids);
+
+  [[nodiscard]] QueryResult::Ptr executeMultiThreaded(Table& table,
+                                                      const std::vector<Table::FieldIndex>& fids);
+
 public:
   using ComplexQuery::ComplexQuery;
 
   QueryResult::Ptr execute() override;
 
   std::string toString() override;
+
+  [[nodiscard]] bool isWriter() const override
+  {
+    return true;
+  }
 };
 
 #endif // PROJECT_SUBQUERY_H
