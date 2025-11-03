833ff2d4ef8168fa62b5a6c921b2e24b8a1a1a49
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-04 01:46:18 +0800

    refactor(p2m2-1): code length

--- src/db/Table.cpp
+++ src/db/Table.cpp
@@ -245,3 +245,24 @@ void Table::completeQuery()
     }
   }
 }
+
+void Table::setName(const std::string& name)
+{
+  this->tableName = name;
+}
+
+const std::string& Table::name() const
+{
+  return this->tableName;
+}
+
+bool Table::empty() const
+{
+  return this->data.empty();
+}
+
+size_t Table::size() const
+{
+  return this->data.size();
+}
+
