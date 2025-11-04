31e77ff5c2f463a9e2a391bf135a61c309685117
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-04 19:17:32 +0800

    fix(p2m2-1): script quality

--- test/linecount.sh
+++ test/linecount.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
-echo "WARNING: Files over 300 lines:"
+VERBOSE=false
+
+for arg in "$@"; do
+    case $arg in
+        --verbose=*)
+            VERBOSE="${arg#*=}"
+            ;;
+    esac
+done
+
+line="WARNING: Files over 300 lines:"
+
+if [ "$VERBOSE" = false ]; then
+    line+=" (Use --verbose or -v to see files over 200 lines)"
+fi
+
+echo "$line"
 find test src -name "*.cpp" -o -name "*.h" | xargs realpath | xargs wc -l | sort -nr | awk '$1 >= 300 && $2 != "total"'
-echo "Files over 200 lines:"
-find test src -name "*.cpp" -o -name "*.h" | xargs realpath | xargs wc -l | sort -nr | awk '$1 >= 200 && $1 < 300 && $2 != "total"'
+if [ "$VERBOSE" = true ]; then
+    echo "Files over 200 lines:"
+    find test src -name "*.cpp" -o -name "*.h" | xargs realpath | xargs wc -l | sort -nr | awk '$1 >= 200 && $1 < 300 && $2 != "total"'
+fi
