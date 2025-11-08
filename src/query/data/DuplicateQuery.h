0ac669177be08ecba01d2beeea43ff5ce74668d5
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-09 01:23:52 +0800

    docs(p2m3-1): add Docs to .h files

--- src/query/data/DuplicateQuery.h
+++ src/query/data/DuplicateQuery.h
@@ -15,10 +15,24 @@ class DuplicateQuery : public ComplexQuery
   using RecordPair = std::pair<Table::KeyType, std::vector<Table::ValueType>>;
 
 private:
+  /**
+   * Validate operands for DUPLICATE query
+   * @return QueryResult indicating validation success or failure
+   */
   [[nodiscard]] QueryResult::Ptr validateOperands() const;
 
+  /**
+   * Execute DUPLICATE operation using single-threaded approach
+   * @param table The table to duplicate records from
+   * @return Vector of record pairs to duplicate
+   */
   [[nodiscard]] std::vector<RecordPair> executeSingleThreaded(Table& table);
 
+  /**
+   * Execute DUPLICATE operation using multi-threaded approach
+   * @param table The table to duplicate records from
+   * @return Vector of record pairs to duplicate
+   */
   [[nodiscard]] std::vector<RecordPair> executeMultiThreaded(Table& table);
 
 public:
