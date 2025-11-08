78d9dd060dd9eb2712714dc9e8318542d0211a42
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-08 23:14:42 +0800

    feat(p2m3-1): stream output pool

--- src/threading/OutputPool.cpp
+++ src/threading/OutputPool.cpp
@@ -4,19 +4,39 @@
 #include <iostream>
 #include <mutex>
 #include <string>
+#include <utility>
+#include <vector>
 
 void OutputPool::addResult(size_t query_id, const std::string& result)
 {
+  //std::cerr << "Adding result for query_id " << query_id << "\n";
   const std::scoped_lock lock(results_mutex);
   results[query_id] = result;
 }
 
-void OutputPool::outputAllResults()
+size_t OutputPool::flushContinuousResults()
 {
-  const std::scoped_lock lock(results_mutex);
-  for (const auto& [query_id, result_string] : results)
+  std::vector<std::pair<size_t, std::string>> ready_results;
   {
-    // Don't print query number for QUIT queries
+    const std::scoped_lock lock(results_mutex);
+    while (true)
+    {
+      const auto iter = results.find(next_output_id);
+      if (iter == results.end())
+      {
+        break;
+      }
+
+      ready_results.emplace_back(iter->first, iter->second);
+      results.erase(iter);
+      ++next_output_id;
+    }
+  }
+
+  const size_t flushed_count = ready_results.size();
+
+  for (const auto& [query_id, result_string] : ready_results)
+  {
     const bool is_quit = result_string.find("QUIT") != std::string::npos;
 
     if (!is_quit) [[unlikely]]
@@ -28,8 +48,21 @@ void OutputPool::outputAllResults()
     {
       std::cout << result_string;
     }
+  }
 
+  if (flushed_count > 0)
+  {
     std::cout.flush();
+  }
+
+  total_output_count.fetch_add(flushed_count, std::memory_order_relaxed);
+  return flushed_count;
+}
+
+void OutputPool::outputAllResults()
+{
+  while (flushContinuousResults() > 0)
+  {
   }
 }
 
@@ -38,3 +71,4 @@ size_t OutputPool::getResultCount() const
   const std::scoped_lock lock(results_mutex);
   return results.size();
 }
+
