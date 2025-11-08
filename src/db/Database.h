ff69b3d4cdf894292d9a21928efec5735b8ad452
Yuzhao Shi <frank20050415@sjtu.edu.cn>
2025-11-09 01:19:30 +0800

    fix(p2m3-1): Code Quality

--- src/db/Database.h
+++ src/db/Database.h
@@ -52,16 +52,39 @@ private:
   Database() = default;
 
 public:
+  /**
+   * Test if a table name is already in use
+   * @param tableName The name of the table to check for duplication
+   */
   void testDuplicate(const std::string& tableName);
 
+  /**
+   * Register a new table in the database
+   * @param table The table to register
+   */
   Table& registerTable(Table::Ptr&& table);
 
+  /**
+   * Drop a table from the database
+   * @param tableName The name of the table to drop
+   */
   void dropTable(const std::string& tableName);
 
+  /**
+   * Print information about all tables
+   */
   void printAllTable();
 
+  /**
+   * Access a table by name (non-const)
+   * @param tableName The name of the table to access
+   */
   [[nodiscard]] Table& operator[](const std::string& tableName);
 
+  /**
+   * Access a table by name (const)
+   * @param tableName The name of the table to access
+   */
   [[nodiscard]] const Table& operator[](const std::string& tableName) const;
 
   Database& operator=(const Database&) = delete;
@@ -74,16 +97,29 @@ public:
 
   ~Database() = default;
 
+  /**
+   * Get the singleton instance of the database
+   */
   [[nodiscard]] static Database& getInstance();
 
+  /**
+   * Update the mapping from file name to table name
+   * @param fileName The file name
+   * @param tableName The table name
+   */
   void updateFileTableName(const std::string& fileName, const std::string& tableName);
 
+  /**
+   * Get the table name associated with a file name
+   * @param fileName The file name to look up
+   * @return The associated table name
+   */
   [[nodiscard]] std::string getFileTableName(const std::string& fileName);
 
   /**
    * Load a table from an input stream (i.e., a file)
-   * @param is
-   * @param source
+   * @param input_stream The stream to read from
+   * @param source Optional source description
    * @return reference of loaded table
    */
   static Table& loadTableFromStream(std::istream& input_stream, const std::string& source = "");
