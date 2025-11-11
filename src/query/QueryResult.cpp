443f5723eb924cc457068971eb24c10d58486559
张涵宁523370910096 <zhang5026871@sjtu.edu.cn>
2025-11-11 21:56:37 +0800

    fix(p2m3-1): cppcoreguidelines fix

--- src/query/QueryResult.cpp
+++ src/query/QueryResult.cpp
@@ -10,3 +10,20 @@ std::ostream& operator<<(std::ostream& out, const QueryResult& table)
 {
   return table.output(out);
 }
+
+static std::string buildMessage(std::string&& msg)
+{
+  return std::move(msg);
+}
+
+static std::string buildMessage(const std::vector<int>& results)
+{
+  std::stringstream stream;
+  stream << "ANSWER = ( ";
+  for (auto result : results)
+  {
+    stream << result << " ";
+  }
+  stream << ")";
+  return stream.str();
+}
