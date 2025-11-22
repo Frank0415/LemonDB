773e3f1c69b9b9e0a0a57edd590ebb8eddb50ad8
张涵宁523370910096 <zhang5026871@sjtu.edu.cn>
2025-11-22 22:50:02 +0800

    fix(p2m3-1): const table for SELECT

--- src/query/data/SelectQuery.h
+++ src/query/data/SelectQuery.h
@@ -23,7 +23,7 @@ private:
    * @return Vector of field indices
    */
   [[nodiscard]] std::vector<Table::FieldIndex>
-  getFieldIndices(Table &table) const;
+  getFieldIndices(const Table &table) const;
 
   /**
    * Execute SELECT operation using single-threaded approach
@@ -32,7 +32,7 @@ private:
    * @return QueryResult with selected records
    */
   [[nodiscard]] QueryResult::Ptr
-  executeSingleThreaded(Table &table,
+  executeSingleThreaded(const Table &table,
                         const std::vector<Table::FieldIndex> &fieldIds);
 
   /**
@@ -42,7 +42,7 @@ private:
    * @return QueryResult with selected records
    */
   [[nodiscard]] QueryResult::Ptr
-  executeMultiThreaded(Table &table,
+  executeMultiThreaded(const Table &table,
                        const std::vector<Table::FieldIndex> &fieldIds);
 
 public:
