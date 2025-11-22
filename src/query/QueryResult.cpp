03ed1c038de64250dd6e95721992653cb724d642
张涵宁523370910096 <zhang5026871@sjtu.edu.cn>
2025-11-22 23:02:52 +0800

    refactor(p2m3-1): clang-format for QueryResult.cpp

--- src/query/QueryResult.cpp
+++ src/query/QueryResult.cpp
@@ -10,7 +10,9 @@ std::ostream &operator<<(std::ostream &out, const QueryResult &table) {
   return table.output(out);
 }
 
-std::string QueryResult::buildMessage(std::string &&msg) { return std::move(msg); }
+std::string QueryResult::buildMessage(std::string &&msg) {
+  return std::move(msg);
+}
 
 std::string QueryResult::buildMessage(const std::vector<int> &results) {
   std::stringstream stream;
