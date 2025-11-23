639c019b86c0fbcd273bfff9f6a37a07dace7d10
张涵宁523370910096 <zhang5026871@sjtu.edu.cn>
2025-11-23 17:43:43 +0800

    refactor(p2m3-1): clang-format for AddQuery.cpp

--- src/query/data/AddQuery.cpp
+++ src/query/data/AddQuery.cpp
@@ -15,7 +15,7 @@
 #include "../../utils/formatter.h"
 #include "../../utils/uexception.h"
 #include "../QueryResult.h"
-#include "./threading/Threadpool.h"
+#include "../../threading/Threadpool.h"
 
 [[nodiscard]] QueryResult::Ptr AddQuery::execute() {
   try {
